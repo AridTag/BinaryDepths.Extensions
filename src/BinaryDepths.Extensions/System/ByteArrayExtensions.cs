b67be7f11f718fac79e442ddd049c20455799dd0
AridTag <AridTag@binarydepths.com>
2018-07-07 04:31:16 -0400

    Add ByteArrayExtensions ToStructUnsafe

---
+++ src/BinaryDepths.Extensions/System/ByteArrayExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace System
+{
+    public static class ByteArrayExtensions
+    {
+        public static unsafe T ToStructUnsafe<T>(this byte[] bytes) where T : struct
+        {
+            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            try
+            {
+                return Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
+            }
+            finally
+            {
+                handle.Free();
+            }
+        }
+
+        public static unsafe T[] ToStructArrayUnsafe<T>(this byte[] bytes, uint numEntries) where T : struct
+        {
+            var sizeOfT = Marshal.SizeOf<T>();
+            var arrayTotalSize = (int)(sizeOfT * numEntries);
+            var array = new T[numEntries];
+            using (var memStream = new MemoryStream(bytes, 0, arrayTotalSize, false))
+            {
+                using (var tableReader = new BinaryReader(memStream))
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        array[i] = tableReader.ReadBytes(sizeOfT).ToStructUnsafe<T>();
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
