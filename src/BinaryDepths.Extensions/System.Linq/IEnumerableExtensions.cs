8ec391962ff40082b08e5d905ed16a7dced2eede
AridTag <AridTag@binarydepths.com>
2018-07-21 18:38:05 -0400

    Add a couple more extensions

---
+++ src/BinaryDepths.Extensions/System.Linq/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool EqualsAny<T>(this T @this, params T[] others)
+        {
+            return others.Contains(@this);
+        }
+
+        public static bool EqualsAny<T>(this T @this, IEqualityComparer<T> comparer, params T[] others)
+        {
+            return others.Contains(@this, comparer);
+        }
+    }
+}
---
+++ src/BinaryDepths.Extensions/System/Int32Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class Int32Extensions
+    {
+        public static int GetNearestPower2Ceiling(this int num)
+        {
+            num = --num | num >> 1;
+            num |= num >> 2;
+            num |= num >> 4;
+            num |= num >> 8;
+            num |= num >> 16;
+            return 1 + num;
+        }
+
+        public static uint GetNearestPower2Ceiling(this uint num)
+        {
+            num = --num | num >> 1;
+            num |= num >> 2;
+            num |= num >> 4;
+            num |= num >> 8;
+            num |= num >> 16;
+            return 1 + num;
+        }
+    }
+}
