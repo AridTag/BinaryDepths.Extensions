164f442fc6c154c956e2d81d3380fa0bd7ff463c
AridTag <AridTag@binarydepths.com>
2018-10-31 20:04:36 -0400

    .

---
+++ src/BinaryDepths.Extensions/System.Diagnostics/ProcessModuleExtensions.cs
@@ -0,0 +1,17 @@
+using System.IO;
+
+namespace System.Diagnostics
+{
+    public static class ProcessModuleExtensions
+    {
+        /// <summary>
+        /// Gets the file name of this process module without the path
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
+        public static string GetFileNameNoPath(this ProcessModule module)
+        {
+            return Path.GetFileName(module.FileName);
+        }
+    }
+}
